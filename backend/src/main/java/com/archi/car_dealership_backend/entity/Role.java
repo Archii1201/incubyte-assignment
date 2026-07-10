cb7c5bc7d3654610462f04110853fcd395a3ade1
Archii1201 <1201archii@gmail.com>
2026-07-11 01:39:08 +0530

    feat: add User entity and repository

---
+++ backend/src/main/java/com/archi/car_dealership_backend/entity/Role.java
@@ -0,0 +1,6 @@
+package com.archi.car_dealership_backend.entity;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ backend/src/main/java/com/archi/car_dealership_backend/entity/User.java
@@ -0,0 +1,38 @@
+package com.archi.car_dealership_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
